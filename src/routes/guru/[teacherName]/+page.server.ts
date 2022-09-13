005b6ef82bf37b0f552e7b303dd7e7554711bf53
falentio <riv.kevinfalent.io@gmail.com>
2022-09-13 23:37:22 +0700

    '-'

--- src/routes/guru/[teacherName]/+page.server.ts
+++ src/routes/guru/[teacherName]/+page.server.ts
@@ -2,12 +2,15 @@ import type { PageServerLoad } from "./$types"
 import { error } from "@sveltejs/kit"
 import { smantaSchedules } from "$lib/smanta"
 
-export const load: PageServerLoad = ({ params }) => {
+export const load: PageServerLoad = ({ params, setHeaders }) => {
 	if (!smantaSchedules.availableTeacher().includes(params.teacherName.replace(/-/gi, " "))) {
 		throw error(404, "teacher not found")
 	}
 	const schedules = smantaSchedules.subjects
 			.filter(i => i.teacher?.replace(/\s/gi, "-") === params.teacherName)
+	setHeaders({
+		"Cache-Control": "public, max-age=7200"
+	})
 	return {
 		schedules,
 	}
--- src/routes/jadwal/[className]/+page.server.ts
+++ src/routes/jadwal/[className]/+page.server.ts
@@ -2,12 +2,17 @@ import type { PageServerLoad } from "./$types"
 import { error } from "@sveltejs/kit"
 import { smantaSchedules } from "$lib/smanta"
 
-export const load: PageServerLoad = ({ params }) => {
+export const prerender = true
+
+export const load: PageServerLoad = ({ params, setHeaders }) => {
 	if (!smantaSchedules.availableClass().includes(params.className.replace(/-/gi, " "))) {
 		throw error(404, "class not found")
 	}
 	const schedules = smantaSchedules.subjects
 			.filter(i => i.class.replace(/\s/gi, "-") === params.className)
+	setHeaders({
+		"Cache-Control": "public, max-age=7200"
+	})
 	return {
 		schedules,
 	}
